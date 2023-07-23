773839183bfe47ca94a874c8ef6aacb0d5a2115a
Michael Pavlovsky <michael.pavlovsky@gmail.com>
2023-07-23 16:52:51 +0000

    simplify the dev experience

--- dotnet-wasi-hello/Program.cs
+++ dotnet-wasi-hello/Program.cs
@@ -1,3 +1,4 @@
 using System;
+using System.Runtime.InteropServices;
 
-Console.WriteLine("Hello, Wasi Console!");
+Console.WriteLine($"Hello, {RuntimeInformation.RuntimeIdentifier} {RuntimeInformation.OSArchitecture}!");
